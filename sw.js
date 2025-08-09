16b1904828f177379be928017b1c7e9bf2d21dfd
Eduardo Chico <eduardo.chico@gmail.com>
2025-08-08 22:05:39 -0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE = "lechuzavsaguilas-v3";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./sw.js",
+  "./assets/owl.svg",
+  "./assets/eagle.svg",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+self.addEventListener("activate", (e) => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k!==CACHE && caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener("fetch", (e) => {
+  const url = new URL(e.request.url);
+  if (url.origin === location.origin) {
+    e.respondWith(caches.match(e.request).then((cached) => cached || fetch(e.request)));
+  } else {
+    e.respondWith(fetch(e.request).catch(() => caches.match("./index.html")));
+  }
+});
